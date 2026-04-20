a70b837b1981a4735ac4c53f6de47ffd2f006e4f
Mario Renan <99850715+mariorenanofc@users.noreply.github.com>
2026-04-20 13:21:34 -0300

    fix(seo): comprime imagem de compartilhamento para corrigir preview no whatsapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,15 +14,25 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Portal Educacional",
-  description: "Plataforma gamificada de gestão de alunos, acompanhamento de missões e ranking.",
+  description:
+    "Plataforma gamificada de gestão de alunos, acompanhamento de missões e ranking.",
   metadataBase: new URL("https://painel-erem.vercel.app"),
 
-  // Configuração do Open Graph (Para WhatsApp, Facebook, Telegram, etc)
+  // CONFIGURAÇÃO EXPLÍCITA DO OPEN GRAPH (Para o Facebook e WhatsApp pararem de reclamar!)
   openGraph: {
-    title: "Portal do Aluno | Área Restrita",
+    title: "Portal Educacional | Área Restrita",
     description: "Acesse suas missões, XP, ranking e muito mais.",
     type: "website",
     url: "https://painel-erem.vercel.app",
+    images: [
+      {
+        // Caminho absoluto apontando para o arquivo na pasta public
+        url: "https://painel-erem.vercel.app/img-share.png",
+        width: 1200,
+        height: 630,
+        alt: "Imagem de visualização do Portal Educacional",
+      },
+    ],
   },
 };
 
